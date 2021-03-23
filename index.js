14d77d522780b846cb68222fbc14ca0e438b5e3a
George An <carbongo@hotmail.com>
2021-03-23 22:11:38 +0900

    ✔ Final: Middleware done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const indexRouter = require("./routes/index");
+const booksRouter = require("./routes/books");
+const userRouter = require("./routes/user");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser());
+app.use(cors());
+
+app.use("", indexRouter);
+app.use("/api/books", booksRouter);
+app.use("/api/user", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
